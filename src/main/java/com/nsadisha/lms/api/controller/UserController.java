a08350b2e88fa4a85152394928afdf4a58a82ec1
Sadisha Nimsara <nsadisha@gmail.com>
2023-01-31 17:23:19 +0530

    return ResponseEntity type responses

--- src/main/java/com/nsadisha/lms/api/controller/UserController.java
+++ src/main/java/com/nsadisha/lms/api/controller/UserController.java
@@ -3,6 +3,8 @@ package com.nsadisha.lms.api.controller;
 import com.nsadisha.lms.api.model.User;
 import com.nsadisha.lms.api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,17 +21,21 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/save")
-    User saveUser(User user) {
-        return userService.saveUser(user);
+    ResponseEntity<User> saveUser(User user) {
+        return ResponseEntity.ok().body(userService.saveUser(user));
     }
 
     @GetMapping("/get")
-    User getUser(@RequestParam String email) {
-        return userService.getUser(email);
+    ResponseEntity<User> getUser(@RequestParam String email) throws UsernameNotFoundException{
+        User user = userService.getUser(email);
+        if(user == null) {
+            throw new UsernameNotFoundException("No user found with "+email);
+        }
+        return ResponseEntity.ok().body(user);
     }
 
     @GetMapping("/all")
-    List<User> getAllUsers() {
-        return userService.getAllUsers();
+    ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAllUsers());
     }
 }
