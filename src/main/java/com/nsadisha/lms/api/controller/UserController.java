78a1aacf622a7c13ac8adb4863f47afb0a1f90ed
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 11:33:20 +0530

    throw exceptions

--- src/main/java/com/nsadisha/lms/api/controller/UserController.java
+++ src/main/java/com/nsadisha/lms/api/controller/UserController.java
@@ -4,10 +4,7 @@ import com.nsadisha.lms.api.model.User;
 import com.nsadisha.lms.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * @author Sadisha Nimsara
@@ -21,16 +18,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/save")
-    ResponseEntity<User> saveUser(User user) {
+    ResponseEntity<User> saveUser(User user) throws Exception{
         return ResponseEntity.ok().body(userService.saveUser(user));
     }
 
     @GetMapping("/get")
-    ResponseEntity<User> getUser(@RequestParam String email) throws UsernameNotFoundException{
+    ResponseEntity<User> getUser(@RequestParam String email) throws Exception{
         User user = userService.getUser(email);
-        if(user == null) {
-            throw new UsernameNotFoundException("No user found with "+email);
-        }
         return ResponseEntity.ok().body(user);
     }
 }
