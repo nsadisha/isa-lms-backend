f0e69571a65be158add12502a871b414ab1db0fa
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 15:41:27 +0530

    add jwt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-devtools:3.0.2")
 
 	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-//	implementation("io.jsonwebtoken:jjwt:0.2")
-//	implementation("io.jsonwebtoken:jjwt-api:0.10.2")
-//	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-//	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
