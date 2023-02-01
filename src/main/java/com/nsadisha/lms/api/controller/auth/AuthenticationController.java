ad57d9e74e5b77491aefc4629299f7432b81a27b
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 17:08:22 +0530

    AuthenticationController initiated

---
+++ src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.nsadisha.lms.api.controller.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 01 of Feb 2023
+ **/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthenticationController {
+}
