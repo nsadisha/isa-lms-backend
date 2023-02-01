425193ae5edf5083c50187e7e205b4ab3facb047
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 12:48:57 +0530

    error response class

--- src/main/java/com/nsadisha/lms/api/exception/ErrorInfo.java
+++ src/main/java/com/nsadisha/lms/api/exception/ErrorInfo.java
@@ -1,22 +1,42 @@
 package com.nsadisha.lms.api.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.security.Timestamp;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * @author Sadisha Nimsara
  * @created 31 of Jan 2023
  **/
 
+@RequiredArgsConstructor
 public class ErrorInfo {
-//    private Timestamp timestamp;
-//    private int status;
-//    private String error;
-    private final String message;
-//    private String path;
+    private final Exception e;
+    private final HttpServletRequest httpReq;
+    private final HttpServletResponse httpRes;
 
-    public ErrorInfo(Exception e) {
-        this.message= e.getLocalizedMessage();
+    Date timestamp = new Date();
+
+    public Map<String, ?> asMap() {
+        return Map.of(
+                "method", httpReq.getMethod(),
+//                "status", httpRes.getStatus(),
+                "path", httpReq.getServletPath(),
+                "timestamp", timestamp,
+                "message", e.getLocalizedMessage()
+        );
+    }
+    public Map<String, ?> asMap(String message) {
+        return Map.of(
+                "method", httpReq.getMethod(),
+//                "status", httpRes.getStatus(),
+                "path", httpReq.getServletPath(),
+                "timestamp", timestamp,
+                "message", message
+        );
     }
 }
