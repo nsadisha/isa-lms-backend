9379f835f1f9317f018b6e4413b47533283cdf09
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 15:43:07 +0530

    initialize student controller

---
+++ src/main/java/com/nsadisha/lms/api/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.nsadisha.lms.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 01 of Feb 2023
+ **/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/student")
+public class StudentController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello from student controller";
+    }
+}
