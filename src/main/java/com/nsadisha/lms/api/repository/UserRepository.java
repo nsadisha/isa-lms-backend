72f14d3a07798ab19a198f64a6538990b914a4a3
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 16:09:15 +0530

    return Optional<User>

--- src/main/java/com/nsadisha/lms/api/repository/UserRepository.java
+++ src/main/java/com/nsadisha/lms/api/repository/UserRepository.java
@@ -4,11 +4,14 @@ import com.nsadisha.lms.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 /**
  * @author Sadisha Nimsara
  * @created 31 of Jan 2023
  **/
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 }
