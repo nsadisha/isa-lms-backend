98ceeb309a7954df7ffa1663c157462a16018c4f
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 12:49:20 +0530

    handling global exception

--- src/main/java/com/nsadisha/lms/api/exception/UserExceptionHandlingController.java
+++ src/main/java/com/nsadisha/lms/api/exception/UserExceptionHandlingController.java
@@ -1,8 +1,11 @@
 package com.nsadisha.lms.api.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,13 +20,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserExceptionHandlingController {
     @ExceptionHandler(UsernameNotFoundException.class)
     @ResponseBody
-    public ResponseEntity<?> handleUserNotFoundException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getLocalizedMessage());
+    public ResponseEntity<?> handleUserNotFoundException(Exception e, HttpServletResponse httpRes, HttpServletRequest httpReq) {
+        ErrorInfo errorInfo = new ErrorInfo(e, httpReq, httpRes);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorInfo.asMap());
     }
 
     @ExceptionHandler(NullUserException.class)
     @ResponseBody
-    public ResponseEntity<?> handleNullUserException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getLocalizedMessage());
+    public ResponseEntity<?> handleNullUserException(Exception e, HttpServletResponse httpRes, HttpServletRequest httpReq) {
+        ErrorInfo errorInfo = new ErrorInfo(e, httpReq, httpRes);
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorInfo.asMap());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public ResponseEntity<?> handleMessageNotReadableException(Exception e, HttpServletResponse httpRes, HttpServletRequest httpReq) {
+        ErrorInfo errorInfo = new ErrorInfo(e, httpReq, httpRes);
+        String message = e.getLocalizedMessage().split(":")[0];
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorInfo.asMap(message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<?> handleGlobalException(Exception e, HttpServletResponse httpRes, HttpServletRequest httpReq) {
+        ErrorInfo errorInfo = new ErrorInfo(e, httpReq, httpRes);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorInfo.asMap());
     }
 }
