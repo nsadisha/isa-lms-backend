b58d03e5057fb3570004633d6ccc353d6474d2d3
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-01 16:08:24 +0530

    implemented with UserDetails

--- src/main/java/com/nsadisha/lms/api/model/User.java
+++ src/main/java/com/nsadisha/lms/api/model/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private int id;
@@ -38,5 +38,35 @@ public class User {
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
