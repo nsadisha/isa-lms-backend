ff6a30a89c307244cc3d6b468205ca763b85a4ea
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 10:53:31 +0530

    class for holding authentication request body

---
+++ src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationRequest.java
@@ -0,0 +1,19 @@
+package com.nsadisha.lms.api.controller.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 01 of Feb 2023
+ **/
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
