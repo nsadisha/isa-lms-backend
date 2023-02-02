beba200a85b7776e552bf020d6338c3f5e064617
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 10:50:10 +0530

    service for register and login users

---
+++ src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationService.java
@@ -0,0 +1,61 @@
+package com.nsadisha.lms.api.controller.auth;
+
+import com.nsadisha.lms.api.filter.JwtService;
+import com.nsadisha.lms.api.model.User;
+import com.nsadisha.lms.api.model.UserFactory;
+import com.nsadisha.lms.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 01 of Feb 2023
+ **/
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final UserFactory userFactory;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .first_name(request.getFirst_name())
+                .last_name(request.getLast_name())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        log.info(user.toString());
+        userRepository.save(userFactory.getInstance(user));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findUserByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Incorrect email address."));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
