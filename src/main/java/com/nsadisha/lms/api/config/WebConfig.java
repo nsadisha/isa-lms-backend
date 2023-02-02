5f70c08a6bf83b0d3b5468e2d353d188e580b77f
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 17:57:46 +0530

    anable cross origin requests

---
+++ src/main/java/com/nsadisha/lms/api/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.nsadisha.lms.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 02 of Feb 2023
+ **/
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
