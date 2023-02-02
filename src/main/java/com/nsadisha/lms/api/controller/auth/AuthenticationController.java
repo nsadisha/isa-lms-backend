32444786a73da2a38e2c2d32a58e0955454ecd7b
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 10:51:55 +0530

    handle /register and /login

--- src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.nsadisha.lms.api.controller.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
