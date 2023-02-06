b62b38c05850471b9d356243b651b47e375bf749
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-06 16:00:28 +0530

    handle runtime exceptions in register and authenticate methods

--- src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationService.java
+++ src/main/java/com/nsadisha/lms/api/controller/auth/AuthenticationService.java
@@ -1,11 +1,14 @@
 package com.nsadisha.lms.api.controller.auth;
 
+import com.nsadisha.lms.api.exception.EmailAlreadyInUseException;
 import com.nsadisha.lms.api.filter.JwtService;
 import com.nsadisha.lms.api.model.User;
 import com.nsadisha.lms.api.model.UserFactory;
 import com.nsadisha.lms.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,34 +28,47 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        User user = User.builder()
-                .first_name(request.getFirst_name())
-                .last_name(request.getLast_name())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(request.getRole())
-                .build();
+    public AuthenticationResponse register(RegisterRequest request) throws Exception{
+        try{
+            User user = User.builder()
+                    .first_name(request.getFirst_name())
+                    .last_name(request.getLast_name())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .role(request.getRole())
+                    .build();
 
-        userRepository.save(userFactory.getInstance(user));
+            userRepository.save(userFactory.getInstance(user));
 
-        var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .user(user)
+                    .build();
+        }catch(DataIntegrityViolationException e){
+            throw new EmailAlreadyInUseException("Your email address is already in use.");
+        }catch(Exception e) {
+            throw new Exception(e.getLocalizedMessage());
+        }
     }
 
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
+    public AuthenticationResponse authenticate(AuthenticationRequest request) throws Exception {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
 
-        User user = userRepository.findUserByEmail(request.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("Incorrect email address."));
+            User user = userRepository.findUserByEmail(request.getEmail()).orElseThrow();
 
-        var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .user(user)
+                    .build();
+        }catch(BadCredentialsException e) {
+            throw new UsernameNotFoundException("Email or password is incorrect.");
+        }catch(Exception e) {
+            throw new Exception(e.getLocalizedMessage());
+        }
     }
 }
