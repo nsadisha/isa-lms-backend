ce0bcf08d59f945d61a85ac2a866cd912763ddf6
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-07 14:44:05 +0530

    exception for validating request body properties

---
+++ src/main/java/com/nsadisha/lms/api/exception/InvalidRequestValueException.java
@@ -0,0 +1,11 @@
+package com.nsadisha.lms.api.exception;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 07 of Feb 2023
+ **/
+public class InvalidRequestValueException extends Exception{
+    public InvalidRequestValueException(String msg) {
+        super(msg);
+    }
+}
