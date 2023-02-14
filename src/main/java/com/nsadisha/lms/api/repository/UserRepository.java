8c213c745f6bdf01fb4135e40298304eb7d1face
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-14 11:51:22 +0530

    optimize imports

--- src/main/java/com/nsadisha/lms/api/repository/UserRepository.java
+++ src/main/java/com/nsadisha/lms/api/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.nsadisha.lms.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 /**
