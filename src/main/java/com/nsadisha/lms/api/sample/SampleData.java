b04e1adc211cf7b1dc1ed9885418cbe97406a6ad
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-22 12:39:24 +0530

    adding sample course data

--- src/main/java/com/nsadisha/lms/api/sample/SampleData.java
+++ src/main/java/com/nsadisha/lms/api/sample/SampleData.java
@@ -2,7 +2,9 @@ package com.nsadisha.lms.api.sample;
 
 import com.nsadisha.lms.api.controller.auth.AuthenticationService;
 import com.nsadisha.lms.api.controller.auth.RegisterRequest;
+import com.nsadisha.lms.api.model.Course;
 import com.nsadisha.lms.api.model.Role;
+import com.nsadisha.lms.api.service.CourseService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,9 +17,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SampleData {
     private final AuthenticationService authenticationService;
+    private final CourseService courseService;
 
     @PostConstruct
-    private void addSampleUsers() throws Exception{
+    private void addSampleUsers() throws Exception {
         authenticationService.register(
                 new RegisterRequest(
                         "Sadisha",
@@ -45,5 +48,29 @@ public class SampleData {
                         Role.MANAGEMENT_STAFF
                 )
         );
+    }
+
+    @PostConstruct
+    private void addSampleCourses() throws Exception {
+        Course course1 = Course.builder()
+                .courseCode("SENG 12345")
+                .name("Test Course 1")
+                .description("Test Description")
+                .build();
+        courseService.createNewCourse(course1, "malshani@gmail.com");
+
+        Course course2 = Course.builder()
+                .courseCode("SENG 12354")
+                .name("Test Course 2")
+                .description("Test Description")
+                .build();
+        courseService.createNewCourse(course2, "malshani@gmail.com");
+
+        Course course3 = Course.builder()
+                .courseCode("SENG 12343")
+                .name("Test Course 3")
+                .description("Test Description")
+                .build();
+        courseService.createNewCourse(course3, "malshani@gmail.com");
     }
 }
