1d62b6ac756fc1f5b67e3466542db8df3d60b5af
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-22 12:07:18 +0530

    course not found exception

---
+++ src/main/java/com/nsadisha/lms/api/exception/CourseNotFoundException.java
@@ -0,0 +1,11 @@
+package com.nsadisha.lms.api.exception;
+
+/**
+ * @author Sadisha Nimsara
+ * @created 22 of Feb 2023
+ **/
+public class CourseNotFoundException extends Exception {
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+}
