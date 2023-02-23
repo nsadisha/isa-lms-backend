3997b660e1495e33da7443f3153b106d4ebe602e
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-23 11:27:59 +0530

    issue with creating new courses fixed.

--- src/main/java/com/nsadisha/lms/api/service/CourseService.java
+++ src/main/java/com/nsadisha/lms/api/service/CourseService.java
@@ -3,13 +3,18 @@ package com.nsadisha.lms.api.service;
 import com.nsadisha.lms.api.exception.CourseCreationFailureException;
 import com.nsadisha.lms.api.exception.CourseNotFoundException;
 import com.nsadisha.lms.api.model.Course;
+import com.nsadisha.lms.api.model.Student;
+import com.nsadisha.lms.api.model.StudentCourseRegistration;
 import com.nsadisha.lms.api.model.Teacher;
 import com.nsadisha.lms.api.repository.CourseRepository;
+import com.nsadisha.lms.api.repository.StudentCourseRegistrationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -19,13 +24,16 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
+@Slf4j
 public class CourseService {
+    private final StudentCourseRegistrationRepository registrationRepository;
     private final CourseRepository courseRepository;
+    private final TeacherService teacherService;
     private final UserService userService;
 
     public Course createNewCourse(Course course, String email) throws Exception {
         try {
-            Teacher conductor = (Teacher) userService.getUser(email);
+            Teacher conductor = teacherService.getTeacher(email);
             course.setConductor(conductor);
             conductor.getConductingCourses().add(course);
             return courseRepository.save(course);
@@ -44,5 +52,34 @@ public class CourseService {
         return courseRepository.findById(id).orElseThrow(
                 () -> new CourseNotFoundException("Cannot find a course with id: "+id)
         );
+    }
+
+    public StudentCourseRegistration enrollStudent(int courseId, String email) throws Exception {
+        try{
+            Student student = (Student) userService.getUser(email);
+            Course course = getCourseById(courseId);
+
+            StudentCourseRegistration registration = StudentCourseRegistration
+                    .builder()
+                    .student(student)
+                    .course(course)
+                    .marks(-1)
+                    .registrationDate(LocalDateTime.now())
+                    .build();
+
+            student.getRegistrations().add(registration);
+//            course.getRegistrations().add(registration); //error
+
+            return registrationRepository.save(registration);
+        }catch(DataIntegrityViolationException e) {
+            throw new CourseCreationFailureException("You have already enrolled in this course.");
+        }catch(Exception e) {
+            throw new CourseCreationFailureException(e.getLocalizedMessage());
+        }
+    }
+
+    public List<Student> getEnrolledStudents(int courseId) throws Exception {
+        Course course = getCourseById(courseId);
+        return course.getRegistrations().stream().map(StudentCourseRegistration::getStudent).toList();
     }
 }
