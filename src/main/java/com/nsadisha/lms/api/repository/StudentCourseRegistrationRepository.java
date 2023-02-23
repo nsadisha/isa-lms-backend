4dfb3e9efb948f579fc713438a0153ca132be3f1
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-23 12:34:54 +0530

    new method to get registration from course and student

--- src/main/java/com/nsadisha/lms/api/repository/StudentCourseRegistrationRepository.java
+++ src/main/java/com/nsadisha/lms/api/repository/StudentCourseRegistrationRepository.java
@@ -1,8 +1,12 @@
 package com.nsadisha.lms.api.repository;
 
+import com.nsadisha.lms.api.model.Course;
+import com.nsadisha.lms.api.model.Student;
 import com.nsadisha.lms.api.model.StudentCourseRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * @author Sadisha Nimsara
@@ -10,4 +14,5 @@ import org.springframework.stereotype.Repository;
  **/
 @Repository
 public interface StudentCourseRegistrationRepository extends JpaRepository<StudentCourseRegistration, Integer> {
+    Optional<StudentCourseRegistration> findStudentCourseRegistrationByCourseAndStudent(Course course, Student student);
 }
