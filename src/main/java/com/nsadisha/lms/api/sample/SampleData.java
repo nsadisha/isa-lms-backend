97c273042d7f8391fc50b366dfc1206a31a06bc7
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-28 12:29:24 +0530

    added some sample course data

--- src/main/java/com/nsadisha/lms/api/sample/SampleData.java
+++ src/main/java/com/nsadisha/lms/api/sample/SampleData.java
@@ -52,26 +52,41 @@ public class SampleData {
 
     @PostConstruct
     private void addSampleCourses() throws Exception {
+        final String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae ante convallis nunc aliquam mattis. Etiam non nisl id elit auctor porta. Vestibulum scelerisque diam at mauris pretium, non aliquam ligula pharetra. Proin pharetra enim eget rhoncus euismod. Suspendisse lacinia eleifend tempus. Pellentesque vulputate efficitur hendrerit. Nam hendrerit lacus diam, vel finibus diam hendrerit vel. Aliquam commodo tortor massa, nec rutrum tortor tristique sed. Phasellus ullamcorper volutpat turpis sit amet venenatis. Aenean sed aliquet quam, ut porttitor augue. Fusce egestas auctor tortor sit amet placerat. Suspendisse potenti. Donec sit amet est metus. Proin consectetur nisi volutpat rutrum viverra. Maecenas facilisis eros non libero volutpat ullamcorper. Aliquam posuere felis in metus rhoncus, eget scelerisque mauris ultrices. ";
         Course course1 = Course.builder()
-                .courseCode("SENG 12345")
-                .name("Test Course 1")
-                .description("Test Description")
+                .courseCode("SENG 11343")
+                .name("Programming Concepts")
+                .description(description)
                 .build();
         courseService.createNewCourse(course1, "malshani@gmail.com");
 
         Course course2 = Course.builder()
-                .courseCode("SENG 12354")
-                .name("Test Course 2")
-                .description("Test Description")
+                .courseCode("SENG 22354")
+                .name("Computer Architecture and Operating Systems")
+                .description(description)
                 .build();
         courseService.createNewCourse(course2, "malshani@gmail.com");
 
         Course course3 = Course.builder()
                 .courseCode("SENG 12343")
-                .name("Test Course 3")
-                .description("Test Description")
+                .name("Data Structures and Algorithms")
+                .description(description)
                 .build();
         courseService.createNewCourse(course3, "malshani@gmail.com");
+
+        Course course4 = Course.builder()
+                .courseCode("SENG 31342")
+                .name("Information Security")
+                .description(description)
+                .build();
+        courseService.createNewCourse(course4, "malshani@gmail.com");
+
+        Course course5 = Course.builder()
+                .courseCode("SENG 22343")
+                .name("Database Design")
+                .description(description)
+                .build();
+        courseService.createNewCourse(course5, "malshani@gmail.com");
     }
 
     @PostConstruct
