cc937016a37c763d0dc7722b91f17356d254dc2c
Sadisha Nimsara <nsadisha@gmail.com>
2023-03-10 16:46:12 +0530

    context loads test

--- src/test/java/com/nsadisha/lms/api/IsaLmsBackendApplicationTests.java
+++ src/test/java/com/nsadisha/lms/api/IsaLmsBackendApplicationTests.java
@@ -1,14 +1,21 @@
 package com.nsadisha.lms.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = IsaLmsBackendApplicationTests.class)
 class IsaLmsBackendApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	void contextLoads() {
-
+		Assertions.assertNotNull(context);
 	}
-
 }
